0a846b7b0a18ad398b3775a5cf65b61f34789ab4
Abhimanyu Manav <71708843+veerabhimanyu@users.noreply.github.com>
2022-01-23 19:57:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import crickFactsRoute from "./routes/facts.js";
+const app = express();
+app.use(bodyParser.json());
+
+const port = 8000;
+
+app.use("/facts", crickFactsRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from cricket facts");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
